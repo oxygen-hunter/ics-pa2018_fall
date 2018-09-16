48e6aea386bf4db1a709cb4c6ac8cdc739dff21f
tracer-pa2018-fall <tracer@njuics.org>
2018-09-16 21:48:21 +0800

    >  nemu 171860699 lixiao Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  21:48:21 up 20:06,  1 user,  load average: 0.15, 0.06, 0.02 b4259250e28e2153721f31198753866661d4e4c

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -268,6 +268,8 @@ void set_OF_adc(uint32_t res, uint32_t src, uint32_t dest, size_t data_size) {
 
 // sub
 void set_CF_sub(uint32_t src, uint32_t dest, size_t data_size) {
+	src = sign_ext(src & (0xFFFFFFFF >> (32 - data_size)), data_size);
+	dest = sign_ext(dest & (0xFFFFFFFF >> (32 - data_size)), data_size);
 	cpu.eflags.CF = dest < src;
 }
 
