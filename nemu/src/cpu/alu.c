07e832be62a3fd5ee093b737a33863ddd3b47a58
monitor <tracer@njuics.org>
2018-09-16 21:11:01 +0800

    monitor test

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -25,7 +25,7 @@ uint32_t alu_adc(uint32_t src, uint32_t dest, size_t data_size) {
 	uint32_t res = 0;
 	res = dest + src + cpu.eflags.CF;
 	
-	set_CF_add(res, dest, data_size);
+	set_CF_adc(res, src, data_size);
 	set_PF(res);
 	//set_AF(); we don't simulate AF
 	set_ZF(res, data_size);
