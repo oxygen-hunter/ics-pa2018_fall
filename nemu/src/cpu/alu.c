05aab494df5bf2e287911bac912390fe830f3393
monitor <tracer@njuics.org>
2018-09-16 23:33:01 +0800

    monitor test

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -279,6 +279,7 @@ void set_CF_adc(uint32_t res, uint32_t src, size_t data_size) {
 }
 
 void set_OF_adc(uint32_t res, uint32_t src, uint32_t dest, size_t data_size) {
+	/*same as add*/
 	set_OF_add(res, src, dest, data_size);
 }
 
@@ -329,13 +330,6 @@ void set_CF_sbb(uint32_t src, uint32_t dest, size_t data_size) {
 }
 
 void set_OF_sbb(uint32_t res, uint32_t src, uint32_t dest, size_t data_size) {
-	/*if old CF = 0, same as sub*/
-	/*if old CF = 1, */
-	/*if(cpu.eflags.CF == 0)*/
-		set_OF_sub(res, src, dest, data_size);
-	//else{
-	//	set_OF_sub(res, src, dest, data_size);
-	//	if(cpu.eflags.CF == 1) return;
-	//	res
-	//}
+	/*same as sub*/
+	set_OF_sub(res, src, dest, data_size);
 }
