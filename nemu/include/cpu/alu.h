a73e2e2652af5731fb0eafa7b665918aae6d8238
monitor <tracer@njuics.org>
2018-09-16 11:37:01 +0800

    monitor test

--- nemu/include/cpu/alu.h
+++ nemu/include/cpu/alu.h
@@ -62,7 +62,7 @@ void set_CF_add(uint32_t res, uint32_t src, size_t data_size);
 void set_PF(uint32_t res);
 void set_ZF(uint32_t res, size_t data_size);
 void set_SF(uint32_t res, size_t data_size);
-void set_OF_add(uint32_t res, uint32_t src, size_t data_size);
+void set_OF_add(uint32_t res, uint32_t src, uint32_t dest, size_t data_size);
 
 
 
