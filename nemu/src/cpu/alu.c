7f9de64b6f8843768f23fe3334afe64fd5b4fe54
tracer-pa2018-fall <tracer@njuics.org>
2018-09-16 20:42:25 +0800

    >  nemu 171860699 lixiao Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  20:42:25 up 19:00,  1 user,  load average: 0.17, 0.06, 0.02 97b23cf94d6a0826b89ad724d20c681551df8aed

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -22,7 +22,7 @@ uint32_t alu_adc(uint32_t src, uint32_t dest, size_t data_size) {
 #ifdef NEMU_REF_ALU
 	return __ref_alu_adc(src, dest, data_size);
 #else
-	uint_32 res = 0;
+	uint32_t res = 0;
 	res = dest + src + cpu.eflags.CF;
 	
 	set_CF_adc(res, src, data_size);
