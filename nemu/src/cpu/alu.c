d2bfa4e2ad9580ced66ed997b2b76b6b5297afff
tracer-pa2018-fall <tracer@njuics.org>
2018-09-16 21:20:06 +0800

    >  nemu 171860699 lixiao Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  21:20:06 up 19:38,  1 user,  load average: 0.13, 0.06, 0.01 ce60d5ca4b492d0a15d05634be05249875e7f666

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -250,5 +250,5 @@ void set_CF_adc(uint32_t res, uint32_t src, uint32_t dest, size_t data_size) {
 	if(cpu.eflags.CF == 1)
 		cpu.eflags.CF = (res <= src);
 	else
-		cpu.eflags.CF = res < sec;
+		cpu.eflags.CF = res < src;
 }
