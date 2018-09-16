84c928f1d3fd08ab60905d76367fb4fe7b9b2e2d
tracer-pa2018-fall <tracer@njuics.org>
2018-09-16 20:43:13 +0800

    >  nemu 171860699 lixiao Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  20:43:13 up 19:01,  1 user,  load average: 0.36, 0.14, 0.05 3a8af6d494e2cc5d3feb4a4bc7c3bbcfc5d64527

--- nemu/include/cpu/alu.h
+++ nemu/include/cpu/alu.h
@@ -58,13 +58,15 @@ inline uint64_t sign_ext_64(uint32_t x, size_t data_size) {
 }
 
 // eflags
+// add
 void set_CF_add(uint32_t res, uint32_t src, size_t data_size);
 void set_PF(uint32_t res);
 void set_ZF(uint32_t res, size_t data_size);
 void set_SF(uint32_t res, size_t data_size);
 void set_OF_add(uint32_t res, uint32_t src, uint32_t dest, size_t data_size);
 
-
+// adc
+void set_CF_adc(uint32_t res, uint32_t src, size_t data_size);
 
 
 
