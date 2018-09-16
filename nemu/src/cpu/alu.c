44e34cc9f11f9351f1de5a1e7268844ca5475ae4
tracer-pa2018-fall <tracer@njuics.org>
2018-09-16 21:14:42 +0800

    >  nemu 171860699 lixiao Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  21:14:42 up 19:33,  1 user,  load average: 0.00, 0.02, 0.00 8189e7442037ddcba5cbda71215ac77e692e9c4c

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -25,7 +25,7 @@ uint32_t alu_adc(uint32_t src, uint32_t dest, size_t data_size) {
 	uint32_t res = 0;
 	res = dest + src + cpu.eflags.CF;
 	
-	set_CF_adc(res, src, data_size);
+	set_CF_adc(res, src, dest, data_size);
 	set_PF(res);
 	//set_AF(); we don't simulate AF
 	set_ZF(res, data_size);
@@ -242,10 +242,10 @@ void set_OF_add(uint32_t res, uint32_t src, uint32_t dest, size_t data_size) {
 	}
 }
 
-void set_CF_adc(uint32_t res, uint32_t src, size_t data_size) {
-	if(cpu.eflags.CF == 1)
-		res -= 1;
+void set_CF_adc(uint32_t res, uint32_t src, uint32_t dest, size_t data_size) {
+
 	res = sign_ext(res & (0xFFFFFFFF >> (32 - data_size)), data_size);
 	src = sign_ext(src & (0xFFFFFFFF >> (32 - data_size)), data_size);
-	cpu.eflags.CF = res < src;
+	dest = sign_ext(dest & (0xFFFFFFFF >> (32 - data_size)), data_size);
+	cpu.eflags.CF = (res < src)||(res < dest)||(res < cpu.eflags.CF);
 }
