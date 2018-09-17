605e8377f3c17c5461a61ca733cd9f70904f6d07
monitor <tracer@njuics.org>
2018-09-17 11:42:02 +0800

    monitor test

--- nemu/include/cpu/alu.h
+++ nemu/include/cpu/alu.h
@@ -78,7 +78,7 @@ void set_CF_sbb(uint32_t src, uint32_t dest, size_t data_size);
 void set_OF_sbb(uint32_t res, uint32_t src, uint32_t dest, size_t data_size);
 
 // mul
-void set_CF_OF_mul(uint32_t src, uint32_t dest, size_t data_size);
+void set_CF_OF_mul(uint64_t res, uint32_t src, uint32_t dest, size_t data_size);
 
 // shl
 void set_CF_shl(uint32_t dest, uint32_t src, size_t data_size);
