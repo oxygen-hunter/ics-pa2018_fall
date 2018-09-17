f08fd9e7fde22a093aa192dc53b53c586b210acb
monitor <tracer@njuics.org>
2018-09-17 09:58:01 +0800

    monitor test

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -212,7 +212,7 @@ uint32_t alu_shl(uint32_t src, uint32_t dest, size_t data_size) {
 	set_SF(res, data_size);
 	//set_OF_shl; we don't simulate OF
 
-	return res;
+	return res&(0xFFFFFFFF >> (32 - data_size));
 #endif
 }
 
