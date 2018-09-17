0f631a33bf577b765b11f561ebcce293e307224f
tracer-pa2018-fall <tracer@njuics.org>
2018-09-17 09:25:49 +0800

    >  nemu 171860699 lixiao Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  09:25:49 up 22:51,  1 user,  load average: 0.01, 0.02, 0.00 e1779a4f73e19a6ce8ebf97ef31485179d9d597d

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -346,8 +346,8 @@ void set_CF_OF_mul(uint32_t src, uint32_t dest, size_t data_size) {
 	/*CF = 0 when high data_size bits of res == 0*/
 	uint64_t res = (uint64_t)(src * dest);
 	uint64_t res64_high32 = res & 0xFFFFFFFF00000000;
-	uint64_t res32_high16 = res & 0x00000000FFFF0000;
-	uint64_t res16_high8 = res & 0x000000000000FF00;
+	uint64_t res32_high16 = res & 0xFFFFFFFFFFFF0000;
+	uint64_t res16_high8 = res & 0xFFFFFFFFFFFFFF00;
 	switch(data_size) {
 		case 8: 
 				if(res16_high8 == 0)
