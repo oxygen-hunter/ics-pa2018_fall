4359cf3c2b1cfca4aa6470c34b92ac4e8c602b9c
tracer-pa2018-fall <tracer@njuics.org>
2018-09-18 14:59:55 +0800

    >  nemu 171860699 lixiao Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  14:59:55 up 1 day,  8:18,  1 user,  load average: 0.00, 0.00, 0.00 93255a2dacd5b9efe60b77e52dd8c561d3859ce4

--- nemu/src/cpu/fpu.c
+++ nemu/src/cpu/fpu.c
@@ -126,7 +126,7 @@ inline uint32_t internal_normalize(uint32_t sign, int32_t exp, uint64_t sig_grs)
 			sig_grs = sig_grs >> 1;
 			sig_grs |= sticky;
 			
-			exp ++;
+			
 		}
 		if(exp < 0) { 
 			/* TODO: assign the number to zero */
