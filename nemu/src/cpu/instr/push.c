fe0cba0e620b2c2115d10fdec68519f3505e55e3
tracer-pa2018-fall <tracer@njuics.org>
2018-10-15 01:02:51 +0800

    >  nemu 171860699 lixiao Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  01:02:51 up 3 days, 16:19,  1 user,  load average: 0.09, 0.03, 0.01 fd8ab6a91bba49f38d879f7ffd3f545c293cce8

--- nemu/src/cpu/instr/push.c
+++ nemu/src/cpu/instr/push.c
@@ -1,8 +1,24 @@
 #include "cpu/instr.h"
 
 make_instr_func(__push_r_v) { //0x50-0x57
-	OPERAND r;
+	OPERAND r, esp;
 
 	r.type = OPR_REG;
-	r.data_size = 
+	r.data_size = data_size;
+	r.addr = opcode & 0x7;
+
+	esp.type = OPR_REG;
+	esp.daya_size = data_size;
+	esp.addr = 4;
+
+	operand_read(&r);
+	operand_read(&esp);
+	esp.val = alu_sub(0x4, esp.val, data_size); //esp -= 4
+	
+	r.type = OPR_MEM; //movl r, (esp)
+	r.addr = esp.val;
+	
+	operand_write(&esp);
+	operand_write(&r);
+	
 }
