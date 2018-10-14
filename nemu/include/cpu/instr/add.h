8175987f78769383f6e8d28091dba02764c2e19d
monitor <tracer@njuics.org>
2018-10-14 23:35:01 +0800

    monitor test

--- nemu/include/cpu/instr/add.h
+++ nemu/include/cpu/instr/add.h
@@ -7,4 +7,7 @@ make_instr_func(__add_rm2r_b); //0x02
 make_instr_func(__add_rm2r_v); //0x03
 make_instr_func(__add_i2a_b); //0x04
 make_instr_func(__add_i2a_v); //0x05
+
+make_instr_func(__add_i2rm_bv); //0x83[0]
+make_instr_func(__adc_i2rm_bv); //0x83[2]
 #endif
