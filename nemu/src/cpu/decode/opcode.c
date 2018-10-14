1f729909f4927626895695a96da46de6b00bccc0
monitor <tracer@njuics.org>
2018-10-14 21:51:01 +0800

    monitor test

--- nemu/src/cpu/decode/opcode.c
+++ nemu/src/cpu/decode/opcode.c
@@ -1,8 +1,8 @@
 #include "cpu/instr.h"
 
 instr_func opcode_entry[256] = {
-/* 0x00 - 0x03*/	inv, inv, inv, inv,
-/* 0x04 - 0x07*/	inv, inv, inv, inv,
+/* 0x00 - 0x03*/	__add_r2rm_b, __add_r2rm_v, __add_rm2r_b, __add_rm2r_v,
+/* 0x04 - 0x07*/	__add_i2a_b, __add_i2a_v, inv, inv,
 /* 0x08 - 0x0b*/	inv, inv, inv, inv,
 /* 0x0c - 0x0f*/	inv, inv, inv, opcode_2_byte,
 /* 0x10 - 0x13*/	inv, inv, inv, inv,
