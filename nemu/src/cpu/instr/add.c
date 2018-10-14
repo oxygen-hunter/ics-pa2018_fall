5d5ca4f08841a878708e9bcf0ca891f2b7c45501
monitor <tracer@njuics.org>
2018-10-14 20:38:01 +0800

    monitor test

--- nemu/src/cpu/instr/add.c
+++ nemu/src/cpu/instr/add.c
@@ -14,6 +14,32 @@ make_instr_func(__add_r2rm_b) { //0x00
 	return len;
 }
 
-make_instr_func(__add_r2rm_v); //0x01
-make_instr_func(__add_rm2r_b); //0x02
+make_instr_func(__add_r2rm_v) { //0x01
+	OPERAND r, rm;
+
+	r.data_size = data_size;
+	rm.data_size = data_size;
+	int len = 1;
+	// read ModR/M for src and dest
+	len += modrm_r_rm(eip + 1, &r, &rm);
+	operand_read(&r);
+	rm.val += r.val;
+	operand_write(&rm);
+	return len;
+}
+
+make_instr_func(__add_rm2r_b) { //0x02
+	OPERAND rm, r;
+
+	r.data_size = 8;
+	rm.data_size = 8;
+	int len = 1;
+	// read ModR/M for src and dest
+	len += modrm_r_rm(eip + 1, &r, &rm);
+	operand_read(&rm);
+	r.val += rm.val;
+	operand_write(&r);
+	return len;
+}
+
 make_instr_func(__add_rm2r_v); //0x03
