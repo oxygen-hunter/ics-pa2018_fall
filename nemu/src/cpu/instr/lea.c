db897bd83e20f63f8e1d1e1daaad28a5aaec8e7f
monitor <tracer@njuics.org>
2018-10-15 19:56:01 +0800

    monitor test

--- nemu/src/cpu/instr/lea.c
+++ nemu/src/cpu/instr/lea.c
@@ -1,8 +1,9 @@
 #include "cpu/instr.h"
 
+/* error: lea instr's len is unknown, so marco is ineffective
 static void instr_execute_2op() {
 	opr_dest.val = opr_src.addr;
 	operand_write(&opr_dest);
-}
+}*/
 
 make_instr_impl_2op(lea, rm, r, v);
