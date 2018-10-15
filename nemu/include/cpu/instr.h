13bb507c7309e3055f9891957df8b50d180a1bf3
tracer-pa2018-fall <tracer@njuics.org>
2018-10-15 16:38:48 +0800

    >  nemu 171860699 lixiao Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  16:38:48 up 3 days, 22:27,  1 user,  load average: 0.01, 0.02, 0.04 6844b2f25efbd6b7a9e4fd13a3d9fadbc390762

--- nemu/include/cpu/instr.h
+++ nemu/include/cpu/instr.h
@@ -33,6 +33,7 @@ extern uint8_t data_size;
 #include "cpu/instr/call.h"
 #include "cpu/instr/ret.h"
 #include "cpu/instr/sbb.h"
+#include "cpu/instr/jcc.h"
 
 
 
--- nemu/include/cpu/instr/jcc.h
+++ nemu/include/cpu/instr/jcc.h
@@ -1,6 +1,6 @@
 #ifndef __INSTR_JCC_H__
 #define __INSTR_JCC_H__
 
-
+make_instr_func(jna_short_); //0x76 jbe
 
 #endif
