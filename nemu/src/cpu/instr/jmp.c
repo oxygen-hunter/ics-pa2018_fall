0fed67dbe422b22093c0e7ef671a87d7d58ddf97
tracer-pa2018-fall <tracer@njuics.org>
2018-10-15 16:26:25 +0800

    >  nemu 171860699 lixiao Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  16:26:25 up 3 days, 22:15,  1 user,  load average: 0.34, 0.20, 0.08 98e2f9ecbaaa805dd026b155170f294720e2f9d6

--- nemu/src/cpu/instr/jmp.c
+++ nemu/src/cpu/instr/jmp.c
@@ -50,7 +50,7 @@ make_instr_func(jna_short_) { //0x76
 	int offset = sign_ext(rel.val, data_size);
 print_asm_1("jbe", "", 2, &rel);
 
-	if(eflags.CF == 1 || eflags.OF == 1)
+	if(cpu.eflags.CF == 1 || cpu.eflags.OF == 1)
 		cpu.eip += offset;
 
 	return 1 + data_size / 8;
