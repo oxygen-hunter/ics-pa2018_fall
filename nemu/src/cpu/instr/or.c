777f754003ee0590b97ac6d3e10e5b8dac14655f
monitor <tracer@njuics.org>
2018-10-15 10:10:01 +0800

    monitor test

--- nemu/src/cpu/instr/or.c
+++ nemu/src/cpu/instr/or.c
@@ -8,3 +8,21 @@ make_instr_func(__or_rm2r_v); //0x0b
 make_instr_func(__or_i2a_b); //0x0c
 make_instr_func(__or_i2a_v); //0x0d
 
+make_instr_func(__or_i2rm_bv) { //0x83[1]
+	OPERAND imm, rm;
+	
+	int len = 1; //opcode len = 1
+	rm.data_size = data_size;
+	len += modrm_rm(eip + 1, &rm);
+	
+	imm.type = OPR_IMM;
+	imm.data_size = 8;
+	imm.addr = eip + len;
+
+	operand_read(&imm); //get imm
+	//rm.val = imm.val | rm.val;
+	rm.val = alu_or(imm.val, rm.val, data_size);
+	operand_write(&rm);
+
+	return len + 1; //opcode + modrm + imm_b
+}
