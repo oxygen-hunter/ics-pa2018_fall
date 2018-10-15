9eb72c2a6cf4cf4bf34ce94a90c4af2cea6208c5
monitor <tracer@njuics.org>
2018-10-15 21:38:01 +0800

    monitor test

--- nemu/include/cpu/instr/adc.h
+++ nemu/include/cpu/instr/adc.h
@@ -1,12 +1,12 @@
 #ifndef __INSTR_ADC_H__
 #define __INSTR_ADC_H__
 
-make_instr_func(adc_r2rm_b); //0x00
-make_instr_func(adc_r2rm_v); //0x01
-make_instr_func(adc_rm2r_b); //0x02
-make_instr_func(adc_rm2r_v); //0x03
-make_instr_func(adc_i2a_b); //0x04
-make_instr_func(adc_i2a_v); //0x05
+make_instr_func(adc_r2rm_b); //0x10
+make_instr_func(adc_r2rm_v); //0x11
+make_instr_func(adc_rm2r_b); //0x12
+make_instr_func(adc_rm2r_v); //0x13
+make_instr_func(adc_i2a_b); //0x14
+make_instr_func(adc_i2a_v); //0x15
 
 make_instr_func(adc_i2rm_bv); //0x83[2]
 
