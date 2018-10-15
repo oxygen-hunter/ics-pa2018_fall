3c968fa75c33bbe4fe0de06521c671835a3800e6
monitor <tracer@njuics.org>
2018-10-15 16:36:01 +0800

    monitor test

--- nemu/include/cpu/instr/jmp.h
+++ nemu/include/cpu/instr/jmp.h
@@ -3,5 +3,5 @@
 
 make_instr_func(jmp_near);
 make_instr_func(jmp_short);
-make_instr_func(jna_short_); //0x76 jbe
+//make_instr_func(jna_short_); //0x76 jbe
 #endif
--- nemu/src/cpu/instr/jmp.c
+++ nemu/src/cpu/instr/jmp.c
@@ -37,7 +37,7 @@ print_asm_1("jmp", "b", 2, &imm);
 	cpu.eip += imm.val;
 	return 2;
 }
-
+/*
 make_instr_func(jna_short_) { //0x76
 	OPERAND rel;
 	rel.type = OPR_IMM;
@@ -55,5 +55,5 @@ print_asm_1("jbe", "", 2, &rel);
 
 	return 1 + data_size / 8;
 
-}
+}*/
 
