f4a3dcb349dfc21550119adc770c0b964b362d21
tracer-pa2018-fall <tracer@njuics.org>
2018-10-15 23:23:32 +0800

    >  nemu 171860699 lixiao Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  23:23:32 up 4 days,  3:39,  1 user,  load average: 0.06, 0.01, 0.00 3066e46671f8f4241b8491592fc6faae5b1f98f8

--- nemu/src/cpu/instr/inc.c
+++ nemu/src/cpu/instr/inc.c
@@ -2,7 +2,7 @@
 
 static void instr_execute_1op() {
 	operand_read(&opr_src);
-	opr_dest.val = alu_add(0x1, opr_src.val, data_size);
+	opr_src.val = alu_add(0x1, opr_src.val, data_size);
 	operand_write(&opr_src);
 }
 
