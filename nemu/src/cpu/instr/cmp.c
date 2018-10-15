50a259e9e4dbc75e93efdb3ebc035b7159177ed5
tracer-pa2018-fall <tracer@njuics.org>
2018-10-15 10:36:53 +0800

    >  nemu 171860699 lixiao Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  10:36:53 up 3 days, 17:37,  1 user,  load average: 0.09, 0.19, 0.13 8445ce4c6cb796002658675b3d3408a587a3eefa

---
+++ nemu/src/cpu/instr/cmp.c
@@ -0,0 +1,20 @@
+#include "cpu/instr.h"
+
+make_instr_func(__cmp_i2rm_bv) { //0x83[7]
+	OPERAND imm, rm;
+	
+	int len = 1; //opcode len = 1
+	rm.data_size = data_size;
+	len += modrm_rm(eip + 1, &rm);
+	
+	imm.type = OPR_IMM;
+	imm.data_size = 8;
+	imm.addr = eip + len;
+
+	operand_read(&imm); //get imm
+	//rm.val - imm.val;
+	alu_sub(imm.val, rm.val, data_size);
+	//operand_write(&rm);
+
+	return len + 1; //opcode + modrm + imm_b
+}
