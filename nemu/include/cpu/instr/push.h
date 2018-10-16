16b5656724a102a27ad1e7d38b32e123f0638d19
monitor <tracer@njuics.org>
2018-10-16 20:37:01 +0800

    monitor test

--- nemu/include/cpu/instr/push.h
+++ nemu/include/cpu/instr/push.h
@@ -2,5 +2,8 @@
 #define __INSTR_PUSH_H__
 
 make_instr_func(push_r_v); //0x50-0x57
+make_instr_func(push_i_b); //0x6a
+make_instr_func(push_i_v); //0x68
+make_instr_func(push_rm_v); //0xff[6]
 make_instr_func(pusha); //0x60
 #endif
