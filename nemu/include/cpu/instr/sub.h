97021dcf4f164213dba0f8b6ca6fcb7a68d3a09d
monitor <tracer@njuics.org>
2018-10-16 17:24:01 +0800

    monitor test

--- nemu/include/cpu/instr/sub.h
+++ nemu/include/cpu/instr/sub.h
@@ -1,7 +1,15 @@
 #ifndef __INSTR_SUB_H__
 #define __INSTR_SUB_H__
 
-make_instr_func(sbb_i2rm_bv); //0x83[3]
-make_instr_func(sub_i2rm_bv); //0x83[5]
+make_instr_func(add_r2rm_b); //0x28
+make_instr_func(add_r2rm_v); //0x29
+make_instr_func(add_rm2r_b); //0x2a
+make_instr_func(add_rm2r_v); //0x2b
+make_instr_func(add_i2a_b); //0x2c
+make_instr_func(add_i2a_v); //0x2d
+
+make_instr_func(add_i2rm_bv); //0x83[5]
+
+make_instr_func(add_i2rm_v); //0x81[5]
 
 #endif
