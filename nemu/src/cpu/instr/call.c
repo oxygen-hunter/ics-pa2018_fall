0236e86e6b3bd707f80eed7015b1aecc88a04e98
tracer-pa2018-fall <tracer@njuics.org>
2018-10-16 21:07:31 +0800

    >  nemu 171860699 lixiao Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  21:07:31 up  2:04,  1 user,  load average: 0.00, 0.02, 0.00 f4342b448c1a443b7d2905d3ea89aa4118ec2a

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -20,7 +20,7 @@ make_instr_func(call_near) {
 //printf("after call's esp:%x\n", cpu.esp);
 
 	int offset = sign_ext(rel.val, data_size);
-print_asm_1("call", "", 2, &rel);
+print_asm_1("call", "", 1 + data_size / 8, &rel);
 
 	cpu.eip += offset;
 
