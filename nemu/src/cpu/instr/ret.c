e14738700a71d16ec7778e951c8d1fc5e4089015
monitor <tracer@njuics.org>
2018-10-16 15:57:01 +0800

    monitor test

--- nemu/src/cpu/instr/ret.c
+++ nemu/src/cpu/instr/ret.c
@@ -10,6 +10,7 @@ make_instr_func(ret_near) { //0xc3
 
 print_asm_0("ret", "", 1);
 	cpu.eip = ret_addr.val; //eip = ret address
+printf("ret's ret address:%x\n", cpu.eip);
 	cpu.esp += data_size / 8; //esp += 4 or 2
 
 	return 0;
