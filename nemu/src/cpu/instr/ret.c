9db9e19d05b1e230ad7bcdf9424ba61db701617d
monitor <tracer@njuics.org>
2018-10-16 15:30:01 +0800

    monitor test

--- nemu/src/cpu/instr/ret.c
+++ nemu/src/cpu/instr/ret.c
@@ -1,18 +1,13 @@
 #include "cpu/instr.h"
 
 make_instr_func(ret_near) { //0xc3
-	OPERAND ret_addr, esp;
-
-	esp.type = OPR_REG;
-	esp.data_size = data_size;
-	esp.addr = 4;
-	operand_read(&esp);
+	OPERAND ret_addr;
 
 	ret_addr.type = OPR_MEM;
 	ret_addr.data_size = data_size;
-	ret_addr.addr = esp.val;
+	ret_addr.addr = cpu.esp;
 	operand_read(&ret_addr);
-//printf("ret addr:%x\n", ret_addr.val);
+
 print_asm_0("ret", "", 1);
 	cpu.eip = ret_addr.val; //eip = ret address
 	esp.val -= 4; //esp -= 4
