15fc5a9dce6be9f423b376a7b41d4988d2828e41
tracer-pa2018-fall <tracer@njuics.org>
2018-10-16 09:48:10 +0800

    >  nemu 171860699 lixiao Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  09:48:10 up 4 days,  5:21,  1 user,  load average: 0.01, 0.05, 0.07 bb5290432e09b418e9e33a69358ee544b656104

--- nemu/include/cpu/instr/jcc.h
+++ nemu/include/cpu/instr/jcc.h
@@ -1,22 +1,22 @@
 #ifndef __INSTR_JCC_H__
 #define __INSTR_JCC_H__
 
-make_instr_func(jo_near); //0x70
-make_instr_func(jno_near); //0x71
-make_instr_func(jb_near); //0x72
-make_instr_func(jae_near); //0x73
-make_instr_func(je_near); //0x74
-make_instr_func(jne_near); //0x75
-make_instr_func(jna_near); //0x76 jbe
-make_instr_func(ja_near); //0x77
-make_instr_func(js_near); //0x78
-make_instr_func(jns_near); //0x79
-make_instr_func(jp_near); //0x7a
-make_instr_func(jnp_near); //0x7b
-make_instr_func(jl_near); //0x7c
-make_instr_func(jge_near); //0x7d
-make_instr_func(jle_near); //0x7e
-make_instr_func(jg_near); //0x7f
+make_instr_func(jo_short_); //0x70
+make_instr_func(jno_short_); //0x71
+make_instr_func(jb_short_); //0x72
+make_instr_func(jae_short_); //0x73
+make_instr_func(je_short_); //0x74
+make_instr_func(jne_short_); //0x75
+make_instr_func(jna_short_); //0x76 jbe
+make_instr_func(ja_short_); //0x77
+make_instr_func(js_short_); //0x78
+make_instr_func(jns_short_); //0x79
+make_instr_func(jp_short_); //0x7a
+make_instr_func(jnp_short_); //0x7b
+make_instr_func(jl_short_); //0x7c
+make_instr_func(jge_short_); //0x7d
+make_instr_func(jle_short_); //0x7e
+make_instr_func(jg_short_); //0x7f
 
 make_instr_func(jo_near); //0x0f 80
 make_instr_func(jno_near); //0x0f 81
