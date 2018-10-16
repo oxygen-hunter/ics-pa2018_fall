690f1e4e440876c890b5090a35a2b533d483734e
monitor <tracer@njuics.org>
2018-10-16 17:22:01 +0800

    monitor test

--- nemu/src/cpu/instr/xor.c
+++ nemu/src/cpu/instr/xor.c
@@ -1,5 +1,24 @@
 #include "cpu/instr.h"
 
+static void instr_execute_2op() {
+	operand_read(&opr_src);
+	operand_read(&opr_dest);
+if(opr_src.data_size < opr_dest.data_size) opr_src.val = sign_ext(opr_src.val, opr_src.data_size);
+//printf("src + dest: %x + %x\n", opr_src.val, opr_dest.val);
+	opr_dest.val = alu_xor(opr_src.val, opr_dest.val, data_size);
+	operand_write(&opr_dest);
+}
+
+make_instr_impl_2op(xor, r, rm, b); //0x00
+make_instr_impl_2op(xor, r, rm, v); //0x01
+make_instr_impl_2op(xor, rm, r, b); //0x02
+make_instr_impl_2op(xor, rm, r, v); //0x03
+make_instr_impl_2op(xor, i, a, b); //0x04
+make_instr_impl_2op(xor, i, a, v); //0x05
+
+make_instr_impl_2op(xor, i, rm, bv); //0x83[6]
+make_instr_impl_2op(xor, i, rm, v); //0x81[6]
+
 /* stupid version...
 make_instr_func(__xor_i2rm_bv) { //0x83[6]
 	OPERAND imm, rm;
