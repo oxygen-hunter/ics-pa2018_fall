78f5234bbda1e0c4c1060da65d2298831c3d36cc
monitor <tracer@njuics.org>
2018-10-16 21:18:01 +0800

    monitor test

--- nemu/include/cpu/instr/setcc.h
+++ nemu/include/cpu/instr/setcc.h
@@ -1,6 +1,21 @@
 #ifndef __INSTR_SETCC_H__
 #define __INSTR_SETCC_H__
 
-
+make_instr_func(set, rm, b, ne)
+make_instr_impl_1op_cc(set, rm, b, a)
+make_instr_impl_1op_cc(set, rm, b, ae)
+make_instr_impl_1op_cc(set, rm, b, be)
+make_instr_impl_1op_cc(set, rm, b, c)
+make_instr_impl_1op_cc(set, rm, b, e)
+make_instr_impl_1op_cc(set, rm, b, g)
+make_instr_impl_1op_cc(set, rm, b, ge)
+make_instr_impl_1op_cc(set, rm, b, l)
+make_instr_impl_1op_cc(set, rm, b, le)
+make_instr_impl_1op_cc(set, rm, b, no)
+make_instr_impl_1op_cc(set, rm, b, np)
+make_instr_impl_1op_cc(set, rm, b, ns)
+make_instr_impl_1op_cc(set, rm, b, o)
+make_instr_impl_1op_cc(set, rm, b, p)
+make_instr_impl_1op_cc(set, rm, b, s)
 
 #endif
