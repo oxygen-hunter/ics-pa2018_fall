e26e58b59dd02b742e968670e07d2768f92e8a5b
monitor <tracer@njuics.org>
2018-10-16 19:32:01 +0800

    monitor test

--- nemu/src/cpu/instr/leave.c
+++ nemu/src/cpu/instr/leave.c
@@ -1,7 +1,7 @@
 #include "cpu/instr.h"
 
 make_instr_func(leave) { //0xc9
-	OPERAND esp, ebp;
+	OPERAND esp, ebp mem;
 
 	esp.type = OPR_REG;
 	esp.data_size = data_size;
@@ -12,8 +12,18 @@ make_instr_func(leave) { //0xc9
 	ebp.addr = 5;
 	operand_read(&ebp);
 
-	esp.val = ebp.val; //mov %ebp, %esp
+	esp.val = ebp.val; //mov %ebp/bp, %esp/sp
 	operand_write(&esp);
+
+	mem.type = OPR_MEM; //pop %ebp/bp
+	mem.data_size = data_size;
+	mem.addr = esp.val;
+	operand_read(&mem);
+
+	ebp.val = mem.val;
+	operand_write(&ebp);
+
+	cpu.esp += data_size / 8;
 print_asm_0("leave", "", 1);
 	return 1;
 }
