e6ab652e5121d740cee9e29dfd960cca5b209670
monitor <tracer@njuics.org>
2018-10-16 16:07:01 +0800

    monitor test

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -1,7 +1,7 @@
 #include "cpu/instr.h"
 
 make_instr_func(call_near) {
-	OPERAND rel, esp, mem;
+	OPERAND rel, mem;
 	rel.type = OPR_IMM;
 	rel.sreg = SREG_CS;
 	rel.data_size = data_size;
@@ -9,19 +9,12 @@ make_instr_func(call_near) {
 
 	operand_read(&rel);
 
-	esp.type = OPR_REG;
-	esp.data_size = data_size;
-	esp.addr = 4; //esp = grp[4]
-	operand_read(&esp);
-
-	//esp.val = alu_sub(0x4, esp.val, data_size); 
 	esp.val -= data_size / 8;
-	operand_write(&esp);
 		
 	mem.type = OPR_MEM; //push ret address
 	mem.data_size = data_size;
 	mem.val = eip + 1 + data_size / 8;
-	mem.addr = esp.val;
+	mem.addr = cpu.esp;
 	operand_write(&mem);
 printf("call's ret addr:%x\n", mem.val);
 //printf("esp:%x\n", esp.val);
