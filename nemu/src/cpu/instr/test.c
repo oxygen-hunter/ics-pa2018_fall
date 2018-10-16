1fa5222842b58e940a82eb55c8ef03a6c993b0a1
tracer-pa2018-fall <tracer@njuics.org>
2018-10-16 23:48:21 +0800

    >  nemu 171860699 lixiao Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  23:48:20 up  3:50,  1 user,  load average: 0.38, 0.15, 0.04 5f26199b54e6013b76354ac7bf3d3bff455a8bda

--- nemu/src/cpu/instr/test.c
+++ nemu/src/cpu/instr/test.c
@@ -10,3 +10,5 @@ make_instr_impl_2op(test, r, rm, b); //0x84
 make_instr_impl_2op(test, r, rm, v); //0x85
 make_instr_impl_2op(test, i, rm, b); //0xf6[0]
 make_instr_impl_2op(test, i, rm, v); //0xf7[0]
+make_instr_impl_2op(test, i, a, b); //0xa8
+make_instr_impl_2op(test, i, a, v); //0xa9
