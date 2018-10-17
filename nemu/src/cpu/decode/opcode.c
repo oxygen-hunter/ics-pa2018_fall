9438e60649d457e47988f950033eb98d61f31465
tracer-pa2018-fall <tracer@njuics.org>
2018-10-17 09:59:00 +0800

    >  nemu 171860699 lixiao Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  09:59:00 up  6:18,  1 user,  load average: 0.19, 0.11, 0.04 fb3e719ba15277ef75fdb1f4701f5e28195622d3

--- nemu/src/cpu/decode/opcode.c
+++ nemu/src/cpu/decode/opcode.c
@@ -36,7 +36,7 @@ instr_func opcode_entry[256] = {
 /* 0x80 - 0x83*/	group_1_b, group_1_v, nemu_trap, group_1_bv,
 /* 0x84 - 0x87*/	test_r2rm_b, test_r2rm_v, inv, inv,
 /* 0x88 - 0x8b*/	mov_r2rm_b, mov_r2rm_v, mov_rm2r_b, mov_rm2r_v,
-/* 0x8c - 0x8f*/	inv, __ref_lea, inv, inv,
+/* 0x8c - 0x8f*/	inv, lea_rm2r_v, inv, inv,
 /* 0x90 - 0x93*/	nop, inv, inv, inv,
 /* 0x94 - 0x97*/	inv, inv, inv, inv,
 /* 0x98 - 0x9b*/	cbw_a_v, cltd, inv, inv,
@@ -113,7 +113,7 @@ instr_func group_3_v_entry[8] =
 
 /* 0xff */
 instr_func group_5_indirect_entry[8] =
-{inc_rm_v, dec_rm_v, inv, inv, inv, inv, push_rm_v, inv};
+{inc_rm_v, dec_rm_v, inv, inv, __ref_jmp_near_indirect, inv, push_rm_v, inv};
 
 instr_func group_7_entry[8] = 
 {inv, inv, inv, inv, inv, inv, inv, inv};
