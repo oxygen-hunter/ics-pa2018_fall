f386d803a862a743eb450d9fa6a588a1c2cac961
tracer-pa2018-fall <tracer@njuics.org>
2018-10-22 00:33:35 +0800

    >  nemu 171860699 lixiao Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  00:33:35 up 12:57,  1 user,  load average: 0.04, 0.08, 0.08 7f544b33a57c2efecf68b6bffcf843277dfff18d

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -14,7 +14,6 @@ make_instr_func(call_near) {
 	mem.type = OPR_MEM; //push ret address
 	mem.data_size = data_size;
 	mem.val = eip + 1 + data_size / 8;
-cpu.esp -= data_size / 8;
 	mem.addr = cpu.esp;
 	operand_write(&mem);
 //printf("call's ret addr:%x\n", mem.val);
