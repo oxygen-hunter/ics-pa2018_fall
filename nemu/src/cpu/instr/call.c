0df5b7465788917554e17231fffacfa47d011cf5
monitor <tracer@njuics.org>
2018-10-22 00:30:01 +0800

    monitor test

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -14,6 +14,7 @@ make_instr_func(call_near) {
 	mem.type = OPR_MEM; //push ret address
 	mem.data_size = data_size;
 	mem.val = eip + 1 + data_size / 8;
+cpu.esp -= data_size / 8;
 	mem.addr = cpu.esp;
 	operand_write(&mem);
 //printf("call's ret addr:%x\n", mem.val);
@@ -25,4 +26,26 @@ print_asm_1("call", "", 1 + data_size / 8, &rel);
 	cpu.eip += offset;
 
 	return 1 + data_size / 8;
+}
+
+make_instr_func(call_near_indirect) { //0xff[2]
+	OPERAND rm, mem;
+	
+	int len = 1;
+	rm.data_size = data_size;
+	
+	len += modrm_rm(eip + 1, &rm);
+
+	operand_read(&rm);
+
+	mem.type = OPR_MEM; //push ret address
+	mem.data_size = data_size;
+	mem.val = eip + len;
+	mem.addr = cpu.esp;
+	operand_write(&mem);
+print_asm_1("call", "", len, &rm);
+
+	cpu.eip = rm.val;
+
+	return 0;
 }
