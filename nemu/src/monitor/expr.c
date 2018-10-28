c5723a2425c5d5cbb5bb79c2a995fdf57c336941
monitor <tracer@njuics.org>
2018-10-28 20:21:01 +0800

    monitor test

--- nemu/src/monitor/expr.c
+++ nemu/src/monitor/expr.c
@@ -98,7 +98,7 @@ static bool make_token(char *e) {
 
 				switch(rules[i].token_type) {
 					case REG: reg_x = which_reg(tokens[nr_token].str); //to judge $xxx is which reg
-							  reg_val_i = cpu.grp[reg_x].val; //get reg's val (uint32_t)
+							  reg_val_i = grp[reg_x].val; //get reg's val (uint32_t)
 							  
 							  memset(reg_val_s, 0, 32);
 							  sprintf(reg_val_s, "%d", reg_val_i); //get reg's val (string)
@@ -209,7 +209,7 @@ int dominant_operator_position(int p, int q) {
 	return 0;
 }
 
-int which_reg(char str) { //str format: $e??
+int which_reg(char* str) { //str format: $e??
 	char str2 = str[2], str3 = str[3];
 	if(str3 == 'x') { //e[a,c,d,b]x
 		if(str2 == 'a') return 0;
