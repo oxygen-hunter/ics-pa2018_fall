ed22af2e8eb620985727e3d50c24ee89ddc91eca
monitor <tracer@njuics.org>
2018-11-12 21:20:02 +0800

    monitor test

--- nemu/include/memory/cache.h
+++ nemu/include/memory/cache.h
@@ -1,5 +1,5 @@
 struct CacheLine {
 	bool valid; //1 when CacheLine is valid, 0 when CacheLine is invalid
-	
-	uint8_t data[64]; //data area who has 64 byte
+	uint32_t sign; //to sign which if this cache matches address
+	uint8_t data[64]; //data area who has 64 B
 }
