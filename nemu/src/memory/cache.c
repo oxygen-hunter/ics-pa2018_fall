86da3478f004b54cb1e0deec70c962ee6d040b96
monitor <tracer@njuics.org>
2018-11-12 21:44:01 +0800

    monitor test

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -4,9 +4,7 @@
 CacheLine cache[CACHELINE_NUM]; //64KB Cache
 
 void init_cache() {
-	for(int i = 0; i < CACHELINE_NUM; i ++) {
-		cache[i].valid = 0;
-	}
+	memset(cache, 0, sizeof(cache));
 }
 
 uint32_t cache_read(paddr_t paddr, size_t len, CacheLine* cache) {
