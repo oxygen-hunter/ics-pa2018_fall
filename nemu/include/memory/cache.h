6d2468597219f5ddc614e82a35f4fc5f1cbcd2db
monitor <tracer@njuics.org>
2018-11-12 21:26:01 +0800

    monitor test

--- nemu/include/memory/cache.h
+++ nemu/include/memory/cache.h
@@ -1,9 +1,11 @@
 #include "nemu.h"
 
+#define CACHE_BLOCK_SIZE 64
+
 struct CacheLine {
 	bool valid; //1 when CacheLine is valid, 0 when CacheLine is invalid
 	uint32_t sign; //to sign which if this cache matches address
-	uint8_t data[64]; //data area who has 64 B
+	uint8_t data[CACHE_BLOCK_SIZE]; //data area who has 64 B
 }
 
 //clear all valid bit
