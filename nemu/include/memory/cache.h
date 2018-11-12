1886bdb011be799cc9925534c95d984023c2eee4
monitor <tracer@njuics.org>
2018-11-12 21:28:01 +0800

    monitor test

--- nemu/include/memory/cache.h
+++ nemu/include/memory/cache.h
@@ -14,3 +14,6 @@ void init_cache();
 
 //read cache
 uint32_t cache_read(paddr_t paddr, size_t len, CacheLine* cache);
+
+//write cache
+void cache_write(paddr_t paddr, size_t len, uint32_t data, CacheLine* cache);
