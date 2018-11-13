f52c3930ff3e61a38b04018404325a44bc57cb9d
monitor <tracer@njuics.org>
2018-11-13 19:05:02 +0800

    monitor test

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -17,22 +17,29 @@ void init_cache() {
 }
 
 uint32_t cache_read(paddr_t paddr, size_t len, CacheLine* cache) {
-	uint32_t tag = paddr >> 13;
-	uint32_t group_index = ((paddr << 19) >> 25) % CACHEGROUP_NUM;
-	uint32_t baddr = paddr & 127;
+	uint32_t tag = paddr >> 13; //high 19 bit
+	uint32_t group_index = ((paddr << 19) >> 25) % CACHEGROUP_NUM; //which group, medium 7 bit
+	uint32_t baddr = paddr & 127; //low 6 bit
 
-	uint32_t group_start = group_index * CACHEGROUP_SIZE;
-	bool group_full = false;
-	for(int i = group_start; i < CACHEGROUP_SIZE; i ++) {
-		if(cache[i].tag == tag) {
-			if(cache[i].valid == 1) {
+	uint32_t group_start = group_index * CACHEGROUP_SIZE; //group start at here
+	bool group_full = false; //if group is full
+	
+	uint32_t result = 0; //the memory i want to read
+	int i = group_start;
+	for(; i < CACHEGROUP_SIZE; i ++) {
+		if(cache[i].tag == tag) { //hit
+			if(cache[i].valid == 1) { //hit, and valid, read it from cache
 				
 			}
-			else {
+			else { //hit, but invalid, copy from disk to cache, then read it 
 
 			}
 			break;
 		}
+	}
+	if(i == CACHEGROUP_SIZE) { //can't hit
+		group_full = true;
+		
 	}
 }
 
