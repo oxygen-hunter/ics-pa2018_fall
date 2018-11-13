c64df395a5c2391b899df0a368fd661e69254663
monitor <tracer@njuics.org>
2018-11-13 16:20:02 +0800

    monitor test

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -6,8 +6,9 @@
 #define c 10
 #define q 8
 #define s 2
-#define CACHEGROUP_NUM (1 << q)
-#define CACHEGROUP_SIZE (1 << s)
+#define b 6
+#define CACHEGROUP_NUM 128
+#define CACHEGROUP_SIZE 8
 
 CacheLine cache[CACHELINE_NUM]; //64KB Cache
 
@@ -16,16 +17,16 @@ void init_cache() {
 }
 
 uint32_t cache_read(paddr_t paddr, size_t len, CacheLine* cache) {
-	uint32_t tag = paddr >> (32 - c);
-	uint32_t group_index = ((paddr << (32 - c)) >> (32 - q)) % CACHEGROUP_NUM;
-	uint32_t gaddr = paddr & (CACHEGROUP_SIZE - 1);
+	uint32_t tag = paddr >> 13;
+	uint32_t group_index = ((paddr << 19) >> 25) % CACHEGROUP_NUM;
+	uint32_t baddr = paddr & 127;
 
 	uint32_t group_start = group_index * CACHEGROUP_SIZE;
 	bool group_full = false;
 	for(int i = group_start; i < CACHEGROUP_SIZE; i ++) {
 		if(cache[i].tag == tag) {
 			if(cache[i].valid == 1) {
-				return
+				
 			}
 			else {
 
