6fa430018ed97a9b2336d438c63411eaa761a983
monitor <tracer@njuics.org>
2018-11-13 22:15:01 +0800

    monitor test

--- nemu/include/memory/cache.h
+++ nemu/include/memory/cache.h
@@ -1,5 +1,4 @@
-//#ifdef CACHE_ENABLED
-//#include "nemu.h"
+#include "nemu.h"
 
 #define CACHE_BLOCK_SIZE 64
 #define CACHELINE_NUM 1024
@@ -21,4 +20,3 @@ uint32_t cache_read(paddr_t paddr, size_t len, CacheLine* cache);
 //write cache
 void cache_write(paddr_t paddr, size_t len, uint32_t data, CacheLine* cache);
 
-//#endif
