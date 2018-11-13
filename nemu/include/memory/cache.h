d88e97320210d5b1dbbca8a124e67e7e1e5b0eaa
monitor <tracer@njuics.org>
2018-11-13 14:46:01 +0800

    monitor test

--- nemu/include/memory/cache.h
+++ nemu/include/memory/cache.h
@@ -5,7 +5,7 @@
 
 struct CacheLine {
 	bool valid; //1 when CacheLine is valid, 0 when CacheLine is invalid
-	uint32_t sign; //to sign which if this cache matches address
+	uint32_t tag; //to sign which if this cache matches address
 	uint8_t data[CACHE_BLOCK_SIZE]; //data area who has 64 B
 }
 
