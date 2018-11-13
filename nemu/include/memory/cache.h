e3d698568bbae46e862a196671d92bbea6634488
tracer-pa2018-fall <tracer@njuics.org>
2018-11-13 20:45:23 +0800

    >  clean 171860699 lixiao Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  20:45:23 up 3 days, 20:02,  1 user,  load average: 0.08, 0.20, 0.11 ed1de0744957ebc8a7478f587f6140ee07943f

--- nemu/include/memory/cache.h
+++ nemu/include/memory/cache.h
@@ -5,11 +5,11 @@
 #define CACHEGROUP_NUM 128
 #define CACHEGROUP_SIZE 8
 
-struct CacheLine {
+typedef struct {
 	bool valid; //1 when CacheLine is valid, 0 when CacheLine is invalid
 	uint32_t tag; //to sign which if this cache matches address
 	uint8_t data[CACHE_BLOCK_SIZE]; //data area who has 64 B
-};
+}CacheLine;
 
 //clear all valid bit
 void init_cache(); 
