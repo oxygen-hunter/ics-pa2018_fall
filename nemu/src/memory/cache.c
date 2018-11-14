4e9c87f6d6bad2536746f46af69dba9f4c15780a
monitor <tracer@njuics.org>
2018-11-14 09:38:01 +0800

    monitor test

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -77,11 +77,11 @@ printf("blank\n");
 			cache[blank_line_index].tag = tag;
 			result = cache_read(paddr, len, cache); //read cache
 printf("blank line:%d\n", blank_line_index);
-for(int m=0;m<64;m++) 
+for(int m=0;m<64;m++) {
 	printf("%x ", cache[blank_line_index].data[m]);
-uint32_t mmm=0;
-memcpy(&mmm, hw_mem+((paddr>>6)<<6),32);
-//printf("\nmmm:%x\n", mmm);
+printf("\n");
+	printf("%x ", hw_mem[((paddr>>6)<<6)+m]);
+}
 printf("\n");
 printf("res:0x%x\n", result);
 			//memcpy(&result, hw_mem + paddr, len);
