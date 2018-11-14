df106ddcf466dff933f6d296222059c8522fd9cf
monitor <tracer@njuics.org>
2018-11-14 17:09:02 +0800

    monitor test

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -77,7 +77,7 @@ uint32_t cache_read(paddr_t paddr, size_t len, CacheLine* cache) {
 }
 
 void cache_write(paddr_t paddr, size_t len, uint32_t data, CacheLine* cache) {
-	memcpy(hw_mem + paddr, &data, len);
+	memcpy(hw_mem + paddr, &data, len); //not write allocate
 	
 	uint32_t tag = paddr >> 13; //high 19 bit
 	uint32_t group_index = (paddr << 19) >> 25; //which group, medium 7 bit
@@ -85,7 +85,7 @@ void cache_write(paddr_t paddr, size_t len, uint32_t data, CacheLine* cache) {
 	uint32_t group_start = group_index * 8; //group start at here
 	for(int i = group_start; i < group_start + 8; i ++) {
 		if(cache[i].tag == tag && cache[i].valid == 1) {
-			cache[i].valid = 0;
+			cache[i].valid = 0; //invalid cacheline
 		}
 	}
 }
