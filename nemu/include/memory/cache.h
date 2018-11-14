17ff7910b6a73501611ef8e650fc768cfa77c796
monitor <tracer@njuics.org>
2018-11-14 11:26:02 +0800

    monitor test

--- nemu/include/memory/cache.h
+++ nemu/include/memory/cache.h
@@ -6,7 +6,7 @@
 #define CACHEGROUP_SIZE 8
 
 typedef struct {
-	bool valid; //1 when CacheLine is valid, 0 when CacheLine is invalid
+	uint8_t valid; //1 when CacheLine is valid, 0 when CacheLine is invalid
 	uint32_t tag; //to sign which if this cache matches address
 	uint8_t data[64]; //data area who has 64 B
 } CacheLine;
