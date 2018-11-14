61676d83b6b71d6119e2a5bcb3bb1a6de4975f3b
tracer-pa2018-fall <tracer@njuics.org>
2018-11-14 09:23:13 +0800

    >  nemu 171860699 lixiao Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  09:23:13 up  1:21,  1 user,  load average: 0.10, 0.07, 0.13 fd34311b1892d530aaa7da8cbc75342b25e2f534

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -74,8 +74,8 @@ printf("blank\n");
 			memcpy(cache[blank_line_index].data, hw_mem + ((paddr >> 6) << 6), 64); //full this blank line
 			cache[blank_line_index].valid = 1;
 			cache[blank_line_index].tag = tag;
-			//result = cache_read(paddr, len, cache); //read cache
-			memcpy(&result, hw_mem + paddr, len);
+			result = cache_read(paddr, len, cache); //read cache
+			//memcpy(&result, hw_mem + paddr, len);
 		}
 		else { //cache group is full
 			int random_num = group_start + 1; //to get a random number in [0:7]
