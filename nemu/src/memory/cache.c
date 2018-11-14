921328fe5d1dbcaba1cb6b92862debfc3c141c76
tracer-pa2018-fall <tracer@njuics.org>
2018-11-14 09:34:58 +0800

    >  nemu 171860699 lixiao Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  09:34:58 up  1:33,  1 user,  load average: 0.10, 0.11, 0.12 a5259d03ec4f08cdb6f106ee39a5ba403a7fa602

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -79,9 +79,9 @@ printf("blank\n");
 printf("blank line:%d\n", blank_line_index);
 for(int m=0;m<64;m++) 
 	printf("%x ", cache[blank_line_index].data[m]);
-uint64_t mmm=0;
-memcpy(&mmm, hw_mem+((paddr>>6)<<6),64);
-printf("\nmmm:%8x\n", mmm);
+uint32_t mmm=0;
+memcpy(&mmm, hw_mem+((paddr>>6)<<6),32);
+printf("\nmmm:%x\n", mmm);
 printf("\n");
 printf("res:0x%x\n", result);
 			//memcpy(&result, hw_mem + paddr, len);
