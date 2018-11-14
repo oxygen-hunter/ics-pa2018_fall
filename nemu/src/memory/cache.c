a9b0016cb5328c4bef680e54fc78b79c218a4807
tracer-pa2018-fall <tracer@njuics.org>
2018-11-14 09:57:42 +0800

    >  nemu 171860699 lixiao Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  09:57:42 up  1:48,  1 user,  load average: 0.43, 0.38, 0.20 c157b077478f53744a18a204344baf4a8fc26a75

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -32,6 +32,7 @@ uint32_t cache_read(paddr_t paddr, size_t len, CacheLine* cache) {
 					memcpy(&mm, hw_mem + paddr, len);
 					if(cc != mm) {
 						printf("i:%d\n", i);
+						printf("baddr:0x%x\n", baddr);
 						printf("incache:%x, inmem:%x\n", cc, mm);
 						assert(0);
 					}
