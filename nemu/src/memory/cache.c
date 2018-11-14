3d152c255f5402e5fae197754fc426e5c672d1a2
tracer-pa2018-fall <tracer@njuics.org>
2018-11-14 08:53:59 +0800

    >  nemu 171860699 lixiao Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  08:53:59 up 52 min,  1 user,  load average: 0.00, 0.11, 0.14 889cf76097b3b8c52b76fa36c3ef3b5f36c26d3

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -34,12 +34,12 @@ printf("directly read from cache\n");
 					memcpy(&result, hw_mem + paddr, len);
 				}
 				else { //cross cacheline to read data
-printf("cross cacheline to read\n");
+printf("cross cacheline to reald\n");
 					uint32_t len_this = 64 - baddr; //length in this line
 					uint32_t len_next = len - len_this; //length in next line
 					uint32_t val_this = 0; //val in this line
 					uint32_t val_next = 0; //val in next line
-					memcpy(&val_this, cache[i].data + baddr, len_this); //read val in this line
+					//memcpy(&val_this, cache[i].data + baddr, len_this); //read val in this line
 					//val_next = cache_read(paddr + len_this, len_next, cache); //read val in next line
 					//result = (val_next << (len_this * 8)) | val_this;
 				 	memcpy(&result, hw_mem + paddr, len);
