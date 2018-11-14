2041f550e628c252a4df60a85eade81e7d359c1f
tracer-pa2018-fall <tracer@njuics.org>
2018-11-14 09:02:22 +0800

    >  clean 171860699 lixiao Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  09:02:22 up  1:00,  1 user,  load average: 0.73, 0.46, 0.28 6ba2396a5750265266638bf63787aafdee5a758

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -86,8 +86,8 @@ uint32_t cache_read(paddr_t paddr, size_t len, CacheLine* cache) {
 			memcpy(cache[random_num].data, hw_mem + ((paddr >> 6) << 6), 64); //replace this line
 			cache[random_num].valid = 1;
 			cache[random_num].tag = tag;
-			//result = cache_read(paddr, len, cache); //read cache
-			memcpy(&result, hw_mem + paddr, len);
+			result = cache_read(paddr, len, cache); //read cache
+			//memcpy(&result, hw_mem + paddr, len);
 		}
 	}
 //printf("result:0x%x\n\n", result);
