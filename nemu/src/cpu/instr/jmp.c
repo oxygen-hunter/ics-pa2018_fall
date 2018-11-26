5eca6f5712f99173645e1cfb617c156432124e4d
monitor <tracer@njuics.org>
2018-11-26 19:42:01 +0800

    monitor test

--- nemu/src/cpu/instr/jmp.c
+++ nemu/src/cpu/instr/jmp.c
@@ -59,7 +59,9 @@ print_asm_1("jmp", "", len, &rm);
 
 make_instr_func(jmp_far_imm) {
 	OPERAND imm;
+	imm.type = OPR_IMM;
 	
+imm.sreg = SREG_CS;
 	imm.addr = eip + 1;
 	imm.data_size = 32;
 	operand_read(&imm);
