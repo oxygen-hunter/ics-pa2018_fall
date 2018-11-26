78d640c4825c22d9ebc3c561eb73931b27a55e33
monitor <tracer@njuics.org>
2018-11-26 23:52:01 +0800

    monitor test

--- nemu/src/memory/mmu/segment.c
+++ nemu/src/memory/mmu/segment.c
@@ -21,7 +21,9 @@ printf("\nload_sreg: sreg:%x\n", sreg);
 	uint32_t index = cpu.segReg[sreg].index; //which SegmentDescriptor
 printf("\nload_sreg: index:%x\n", index);
 	SegDesc* segTable = (SegDesc*) cpu.gdtr.base; //segTable start addr
-printf("\nload_sreg: segTable:%x\n", (uint32_t)segTable);
+	SegDesc.val[0] = hw_mem_read(segTable + 8 * index, 4);
+	SegDesc.val[1] = hw_mem_read(segTable + 8 * index + 4, 4);
+//printf("\nload_sreg: segTable:%x\n", (uint32_t)segTable);
 	//assert(segTable[index].base_15_0 == 0x0); //check SegmentDescriptor
 	//assert(segTable[index].limit_15_0 == 0xffff);
 	//assert(segTable[index].granularity == 0x1);
