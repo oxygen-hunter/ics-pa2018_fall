3924c024367f73e26b46b78b72e1c7a44e4b937f
monitor <tracer@njuics.org>
2018-11-26 23:54:01 +0800

    monitor test

--- nemu/src/memory/mmu/segment.c
+++ nemu/src/memory/mmu/segment.c
@@ -17,9 +17,9 @@ void load_sreg(uint8_t sreg) {
 	/* TODO: load the invisibile part of the segment register 'sreg' by reading the GDT.
 	 * The visible part of 'sreg' should be assigned by mov or ljmp already.
 	 */
-printf("\nload_sreg: sreg:%x\n", sreg);
+//printf("\nload_sreg: sreg:%x\n", sreg);
 	uint32_t index = cpu.segReg[sreg].index; //which SegmentDescriptor
-printf("\nload_sreg: index:%x\n", index);
+//printf("\nload_sreg: index:%x\n", index);
 	laddr_t segTable = (laddr_t) cpu.gdtr.base; //segTable start addr, is a laddr
 	SegDesc.val[0] = laddr_read(segTable + 8 * index, 4);
 	SegDesc.val[1] = laddr_read(segTable + 8 * index + 4, 4);
@@ -27,9 +27,9 @@ printf("\nload_sreg: index:%x\n", index);
 	//assert(segTable[index].base_15_0 == 0x0); //check SegmentDescriptor
 	//assert(segTable[index].limit_15_0 == 0xffff);
 	//assert(segTable[index].granularity == 0x1);
-printf("load_sreg: sizeof():%x\n", sizeof(SegDesc));
-printf("load_sreg: segTable[index].val[0]:%x, val[1]:%x\n", segTable[index].val[0], segTable[index].val[1]);
-	cpu.segReg[sreg].base = segTable[index].base_15_0; //load sreg's invisible part
-	cpu.segReg[sreg].limit = segTable[index].limit_15_0;
+//printf("load_sreg: sizeof():%x\n", sizeof(SegDesc));
+//printf("load_sreg: segTable[index].val[0]:%x, val[1]:%x\n", segTable[index].val[0], segTable[index].val[1]);
+	cpu.segReg[sreg].base = SegDesc.base_15_0; //load sreg's invisible part
+	cpu.segReg[sreg].limit = SegDesc.limit_15_0;
 printf("3\n");
 }
