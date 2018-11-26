ffce4396e699c1ef7f0e9dc96b8336de74d82e6d
monitor <tracer@njuics.org>
2018-11-26 17:10:01 +0800

    monitor test

--- nemu/src/cpu/instr/lgdt.c
+++ nemu/src/cpu/instr/lgdt.c
@@ -4,6 +4,7 @@ make_instr_func(lgdt) {
 	OPERAND rm;
 	rm.data_size = data_size;
 	modrm_rm(eip + 1, &rm);
+	operand_read(&rm);
 	uint32_t base = rm.val;
 	uint32_t limit = rm.val & 0xffff;
 	cpu.gdtr.base = base;
