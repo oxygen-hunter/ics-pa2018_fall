02ac835a1825c9bf25e59d9316a1e061a5d28851
monitor <tracer@njuics.org>
2018-11-27 15:48:01 +0800

    monitor test

--- nemu/src/cpu/instr/leave.c
+++ nemu/src/cpu/instr/leave.c
@@ -5,11 +5,13 @@ make_instr_func(leave) { //0xc9
 
 	esp.type = OPR_REG;
 	esp.data_size = data_size;
+esp.sreg = SREG_SS; //2018.11.27 15:46
 	esp.addr = 4;
 
 	ebp.type = OPR_REG;
 	ebp.data_size = data_size;
 	ebp.addr = 5;
+ebp.sreg = SREG_SS; //2018.11.27 15:46
 	operand_read(&ebp);
 
 	esp.val = ebp.val; //mov %ebp/bp, %esp/sp
@@ -18,6 +20,7 @@ make_instr_func(leave) { //0xc9
 	mem.type = OPR_MEM; //pop %ebp/bp
 	mem.data_size = data_size;
 	mem.addr = esp.val;
+mem.sreg = SREG_DS; //2018.11.27 15:46
 	operand_read(&mem);
 
 	ebp.val = mem.val;
