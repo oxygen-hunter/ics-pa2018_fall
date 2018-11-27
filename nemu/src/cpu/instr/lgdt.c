1736c93f67b202babd6d40d14a74e7e193998daa
tracer-pa2018-fall <tracer@njuics.org>
2018-11-27 15:14:54 +0800

    >  nemu 171860699 lixiao Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  15:14:53 up 1 day,  4:11,  1 user,  load average: 0.04, 0.02, 0.00 ffa8e82ad6665d2d9d194b01738b906e1e81c193

--- nemu/src/cpu/instr/lgdt.c
+++ nemu/src/cpu/instr/lgdt.c
@@ -7,10 +7,12 @@ make_instr_func(lgdt) {
 	int len = 1;
 	len += modrm_rm(eip + 1, &rm);
 	operand_read(&rm);
-	uint32_t base = rm.val;
+	/*uint32_t base = rm.val;
 	uint32_t limit = rm.val & 0xffff; //low 16 bit
 	cpu.gdtr.base = base;
-	cpu.gdtr.limit = limit;
+	cpu.gdtr.limit = limit;*/
+	printf("addr:%x\n", rm.addr);
+	printf("val:%x\n", rm.val);
 print_asm_1("lgdt", "", len, &rm);
 	return len;
 }
