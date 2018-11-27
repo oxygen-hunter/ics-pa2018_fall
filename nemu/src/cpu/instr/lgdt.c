0dbaaaba40b86e26d93f51df98147634671e3652
tracer-pa2018-fall <tracer@njuics.org>
2018-11-27 15:29:42 +0800

    >  nemu 171860699 lixiao Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  15:29:42 up 1 day,  4:26,  1 user,  load average: 0.13, 0.06, 0.02 63388d71e6f06671574b02713d432290516e9bcb

--- nemu/src/cpu/instr/lgdt.c
+++ nemu/src/cpu/instr/lgdt.c
@@ -3,23 +3,21 @@
 
 make_instr_func(lgdt) {
 	OPERAND rm;
-	rm.data_size = 16;
 
 	int len = 1;
 	len += modrm_rm(eip + 1, &rm);
-	operand_read(&rm);
-	/*uint32_t base = rm.val;
-	uint32_t limit = rm.val & 0xffff; //low 16 bit
-	cpu.gdtr.base = base;
-	cpu.gdtr.limit = limit;*/
-	printf("addr:%x\n", rm.addr);
-	printf("val:%x\n", rm.val);
 	
-	cpu.gdtr.limit = rm.val;
-	rm.addr = rm.addr + 2*8;
+	rm.data_size = 16;
+	operand_read(&rm);
+
+printf("addr:%x\n", rm.addr);
+printf("val:%x\n", rm.val);
+	
+	cpu.gdtr.limit = rm.val; // load limit
+	rm.addr = rm.addr + 2*8; // addr + 16byte
 	rm.data_size = 32;
 	operand_read(&rm);
-	cpu.gdtr.base = rm.val;
+	cpu.gdtr.base = rm.val; //load base
 print_asm_1("lgdt", "", len, &rm);
 	return len;
 }
