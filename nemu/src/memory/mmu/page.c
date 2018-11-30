ec04c222cc16ff7146efc29c8b5e69f50320891e
monitor <tracer@njuics.org>
2018-11-30 11:39:01 +0800

    monitor test

--- nemu/src/memory/mmu/page.c
+++ nemu/src/memory/mmu/page.c
@@ -6,11 +6,18 @@ paddr_t page_translate(laddr_t laddr) {
 #ifndef TLB_ENABLED
 	uint32_t dir = laddr >> 22; // page dirctory index. high 10 bit
 	uint32_t page = laddr << 10 >> 22; // page table index, mid 10 bit
-	uint32_t offset = laddr << 22 >> 22; // page frame index, low 12 bit
+	uint32_t offset = laddr << 20 >> 20; // page frame index, low 12 bit
 	
-	uint32_t PDE_start = cpu.cr3.pdtr + dir; // page directory start here. physical addr
-	uint32_t PTE_start = PDE_start + dir; //page table start here.
-	uint32_t 
+	paddr_t PDE_start = cpu.cr3.pdtr + 8 * dir; // page directory start here. physical addr
+	PDE pde;
+	pde.val = paddr_read(PDE_start, 4); // read PDE
+
+	paddr_t PTE_start = pde.page_frame + 8 * page; // page table start here.
+	PTE pte;
+	pte.val = paddr_read(PTE_start, 4); // read PTE
+
+	paddr_t paddr = pte.page_frame + 8 * offset; // physical addr.
+	return paddr;
 #else	
 	return tlb_read(laddr) | (laddr & PAGE_MASK);;
 #endif
