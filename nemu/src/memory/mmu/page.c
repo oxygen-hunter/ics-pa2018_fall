4ade03621d39de97d13540226017ec3927198af5
monitor <tracer@njuics.org>
2018-12-01 00:53:01 +0800

    monitor test

--- nemu/src/memory/mmu/page.c
+++ nemu/src/memory/mmu/page.c
@@ -9,7 +9,7 @@ printf("laddr:%x\n", laddr);
 	uint32_t page = (laddr << 10) >> 22; // page table index, mid 10 bit
 	uint32_t offset = (laddr << 20) >> 20; // page frame index, low 12 bit
 	
-	paddr_t PDE_start = cpu.cr3.pdbr + 4 * dir; // page directory start here. physical addr
+	paddr_t PDE_start = (cpu.cr3.pdbr << 12) + 4 * dir; // page directory start here. physical addr
 printf("PDE_start:%x\n", PDE_start);
 	PDE pde;
 	pde.val = paddr_read(PDE_start, 4); // read PDE
@@ -19,6 +19,7 @@ printf("pde.val:%x\n", pde.val);
 	paddr_t PTE_start = pde.page_frame + 4 * page; // page table start here.
 	PTE pte;
 	pte.val = paddr_read(PTE_start, 4); // read PTE
+printf("pte.val:%x\n", pte.val);
 	assert(pte.present == 1);
 
 	paddr_t paddr = pte.page_frame + offset; // physical addr.
