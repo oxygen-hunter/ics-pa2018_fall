48cc1a0a7abce0de00c12b6e218f8da1589822b8
tracer-pa2018-fall <tracer@njuics.org>
2018-11-30 16:43:19 +0800

    >  nemu 171860699 lixiao Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  16:43:19 up  2:56,  1 user,  load average: 0.22, 0.14, 0.10 fa427242ed96ef88d5cbeeb3ffd215a7a917b5b6

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -41,7 +41,7 @@ uint32_t loader() {
 	Log("777\n");
 			memcpy((void*)(mm_paddr), (void*)(ph->p_offset), ph->p_filesz);
 			if(ph->p_memsz > ph->p_filesz)
-				memset((void*)(hw_mem + mm_paddr+ph->p_filesz), 0, ph->p_memsz - ph->p_filesz);
+				memset((void*)(mm_paddr+ph->p_filesz), 0, ph->p_memsz - ph->p_filesz);
 //#else
 //			memcpy((void*)(ph->p_vaddr), (void*)(ph->p_offset), ph->p_filesz);
 //			/* TODO: zeror the memory area [vaddr + file_sz, vaddr + mem_sz] */
