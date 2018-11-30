a0960eb268f23d3fba78635bcbc35a75053d4470
tracer-pa2018-fall <tracer@njuics.org>
2018-11-30 21:14:07 +0800

    >  nemu 171860699 lixiao Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  21:14:07 up  5:58,  1 user,  load average: 0.00, 0.00, 0.00 5c3399d42b6c5f1c531c60cc810ebfccc94090d4

--- nemu/src/cpu/instr/mov.c
+++ nemu/src/cpu/instr/mov.c
@@ -322,9 +322,9 @@ make_instr_func(mov_r2c_l) {
 
 	int len = 1;
 	len += modrm_r_rm(eip + len, &cr, &r);
-	cr.type = OPR_CREG;
 printf("c2r:after modrm, cr.type:%x, cr.addr:%x\n", cr.type, cr.addr);
 //if(cr.type != OPR_CREG) assert(0);
+	cr.type = OPR_CREG;
 	operand_read(&r);
 	cr.val = r.val;
 	operand_write(&cr);
