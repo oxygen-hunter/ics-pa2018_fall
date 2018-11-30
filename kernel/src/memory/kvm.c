26fe26030c9d9055ca4484794f81e2626edf64ae
tracer-pa2018-fall <tracer@njuics.org>
2018-11-30 21:26:47 +0800

    >  clean 171860699 lixiao Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  21:26:47 up  6:11,  1 user,  load average: 0.12, 0.08, 0.02 aef0125b2f2e1e3778f241c7dbed027bfa406231

--- kernel/src/memory/kvm.c
+++ kernel/src/memory/kvm.c
@@ -41,8 +41,6 @@ void init_page(void) {
 	cr0.val = read_cr0();
 	cr0.paging = 1;
 	write_cr0(cr0.val);
-	Log("write_cr0 already\n");
-	assert(0);
 }
 
 /* GDT in the kernel's memory, whose virtual memory is greater than 0xC0000000. */
