3689c519e930e5a8abd0b2483d574eceefdf9f1d
tracer-pa2018-fall <tracer@njuics.org>
2018-12-01 00:40:11 +0800

    >  clean 171860699 lixiao Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  00:40:11 up  7:06,  1 user,  load average: 0.12, 0.08, 0.06 236d8dd88719db88b896f475c9ee5b16a9c7c925

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -36,16 +36,15 @@ uint32_t loader() {
 			//panic("Please implement the loader"); //annotate this sentence after finish loader()
 
 			/* TODO: copy the segment from the ELF file to its proper memory area */
-#ifdef IA32_PAGE
 			void* mm_paddr = (void*)mm_malloc((uint32_t)ph->p_vaddr, ph->p_memsz);
 			memcpy(mm_paddr, (void*)(ph->p_offset), ph->p_filesz);
 			if(ph->p_memsz > ph->p_filesz)
 				memset((void*)(mm_paddr+ph->p_filesz), 0, ph->p_memsz - ph->p_filesz);
-#else
-			memcpy((void*)(ph->p_vaddr), (void*)(ph->p_offset), ph->p_filesz);
-			/* TODO: zeror the memory area [vaddr + file_sz, vaddr + mem_sz] */
-			if(ph->p_memsz > ph->p_filesz)
-				memset((void*)(ph->p_vaddr+ph->p_filesz), 0, ph->p_memsz - ph->p_filesz);
+
+//			memcpy((void*)(ph->p_vaddr), (void*)(ph->p_offset), ph->p_filesz);
+//			/* TODO: zeror the memory area [vaddr + file_sz, vaddr + mem_sz] */
+//			if(ph->p_memsz > ph->p_filesz)
+//				memset((void*)(ph->p_vaddr+ph->p_filesz), 0, ph->p_memsz - ph->p_filesz);
 #endif
 #ifdef IA32_PAGE
 			/* Record the program break for future use */
