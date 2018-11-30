eae70cdf3258d15f5cf7f344fc53bfb10b7aa634
tracer-pa2018-fall <tracer@njuics.org>
2018-11-30 16:55:23 +0800

    >  nemu 171860699 lixiao Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  16:55:23 up  3:08,  1 user,  load average: 0.16, 0.08, 0.06 3be12073cc5eb6e1fceb2b1168ae5191cf12dfd3

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -39,6 +39,7 @@ uint32_t loader() {
 //#ifdef IA32_PAGE
 			uint32_t mm_paddr = mm_malloc((uint32_t)ph->p_vaddr, ph->p_memsz);
 	Log("777\n");
+	assert(0);
 			memcpy((void*)(mm_paddr), (void*)(ph->p_offset), ph->p_filesz);
 			if(ph->p_memsz > ph->p_filesz)
 				memset((void*)(mm_paddr+ph->p_filesz), 0, ph->p_memsz - ph->p_filesz);
