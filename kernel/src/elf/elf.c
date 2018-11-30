d7c1d14cb7c7d64fc35f93e044b95211acfec333
monitor <tracer@njuics.org>
2018-11-30 15:55:01 +0800

    monitor test

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -37,8 +37,8 @@ uint32_t loader() {
 
 			/* TODO: copy the segment from the ELF file to its proper memory area */
 //#ifdef IA32_PAGE
-			void* mm_paddr = (void*)mm_malloc((uint32_t)ph->p_vaddr, ph->p_memsz);
-			memcpy(mm_paddr, (void*)(ph->p_offset), ph->p_filesz);
+			uint32_t mm_paddr = mm_malloc((uint32_t)ph->p_vaddr, ph->p_memsz);
+			memcpy((void*)(hw_mem + mm_paddr), (void*)(ph->p_offset), ph->p_filesz);
 			if(ph->p_memsz > ph->p_filesz)
 				memset((void*)(mm_paddr+ph->p_filesz), 0, ph->p_memsz - ph->p_filesz);
 //#else
