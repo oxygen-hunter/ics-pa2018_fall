3cb8f878718737bc51340dd95fa200e3f2809011
tracer-pa2018-fall <tracer@njuics.org>
2018-12-17 21:53:37 +0800

    >  nemu 171860699 lixiao Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  21:53:37 up 22:06,  1 user,  load average: 0.02, 0.05, 0.00 6173588b1cbce167e888104d40de5d9dab2c94d

---
+++ nemu/src/cpu/instr/lidt.c
@@ -0,0 +1,21 @@
+#include "cpu/instr.h"
+#include "memory/memory.h"
+
+make_instr_func(lgdt) {
+	OPERAND rm;
+
+	int len = 1;
+	len += modrm_rm(eip + 1, &rm);
+	
+	rm.data_size = 16;
+	operand_read(&rm);
+	
+	cpu.idtr.limit = rm.val; // load limit
+	rm.addr = rm.addr + 2; // addr + 16bit
+	rm.data_size = 32;
+	operand_read(&rm);
+	cpu.idtr.base = rm.val; //load base
+
+print_asm_1("lidt", "", len, &rm);
+	return len;
+}
