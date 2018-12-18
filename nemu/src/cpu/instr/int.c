20d70fdfea8d6f1edc285096161fd6e20c82ea52
monitor <tracer@njuics.org>
2018-12-18 16:41:02 +0800

    monitor test

--- nemu/src/cpu/instr/int.c
+++ nemu/src/cpu/instr/int.c
@@ -5,10 +5,10 @@ make_instr_func(int_) {
 	OPERAND intr_no;
 	
 	intr_no.type = OPR_IMM;
-	intr_no.data_size = 8; //uint8_t
+	intr_no.data_size = 8; // uint8_t
 	intr_no.addr = eip + 1;
 	
-	operand_read(&intr_no);
+	operand_read(&intr_no); // get intr_no
 
 print_asm_1("int", "", 2, &intr_no);
 	
