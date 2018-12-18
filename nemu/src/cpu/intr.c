a39dc985d3250d683cbfdc3ac59e167292f898f7
monitor <tracer@njuics.org>
2018-12-18 16:57:01 +0800

    monitor test

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -5,8 +5,27 @@
 
 void raise_intr(uint8_t intr_no) {
 #ifdef IA32_INTR
-	printf("Please implement raise_intr()");
-	assert(0);
+	//printf("Please implement raise_intr()");
+	//assert(0);
+
+	// Trigger an exception/interrrupt with 'intr_no'
+	// 'intr_no' is the index to the IDT
+	
+
+	// Push EFLAGS, CS and EIP
+	
+
+	// Find the IDT entry using 'intr_no'
+	GateDesc gatedesc;
+	laddr_t IDT_start = cpu.idtr.base + 8 * intr_no; // idt start here. laddr
+	gatedesc.val = laddr_read(IDT_start, 8); // read gate descripter' val
+
+	// Clear IF if it is an interrupt
+	if(interrupt)
+		cpu.eflags.IF = 0;
+
+	// Set EIP to the entry of the interrut handler
+	cpu.eip = 
 #endif
 }
 
