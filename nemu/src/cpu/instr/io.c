2b3cd4a1481bf296f49b9aec7f0da348405a08de
monitor <tracer@njuics.org>
2018-12-22 11:51:02 +0800

    monitor test

--- nemu/src/cpu/instr/io.c
+++ nemu/src/cpu/instr/io.c
@@ -29,9 +29,38 @@ make_instr_func(in_v) {
 	ax.type = OPR_REG;
 	ax.data_size = data_size; //%eax/%ax
 	ax.addr = 0;
-	ax.val = pio_read(dx.val, data_size / 8); //Input byte from port DX into %eax/%ax
+	ax.val = pio_read(dx.val, data_size / 8); //Input dword/word from port DX into EAX/AX
 	operand_write(&ax);
 if(data_size == 16) print_asm_0("in %dx, %ax", "", 1);
 else print_asm_0("in %dx, %eax", "", 1);
 	return 1;
 }
+
+make_instr_func(out_b) {
+	OPERAND al, dx;
+	
+	al.type = OPR_REG;
+	al.data_size = 8; //%al
+	al.addr = 0;
+	operand_read(&al); //get %al
+
+	dx.type = OPR_REG;
+	dx.data_size = 16; //%dx
+	dx.addr = 2;
+	operand_read(&dx); //get %dx
+
+	pio_write(dx.val, 1, al.val); //Output byte AL to port DX
+
+print_asm_0("in %dx, %al", "", 1);
+	return 1;
+	
+}
+
+
+
+
+
+
+
+
+
