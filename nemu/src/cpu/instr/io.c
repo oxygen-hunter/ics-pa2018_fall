4a9729a11847604bbceb9382c03c3345dcd0f61b
monitor <tracer@njuics.org>
2018-12-22 11:34:01 +0800

    monitor test

--- nemu/src/cpu/instr/io.c
+++ nemu/src/cpu/instr/io.c
@@ -1,14 +1,16 @@
 #include "cpu/instr.h"
 
-make_instr_func(out_b) {
-	OPERAND AL, DX;
+make_instr_func(in_b) {
+	OPERAND al, dx;
 	
-	AL.type = OPR_REG;
-	AL.data_size = 8;
-	AL.addr = 0;
-	
-	DX.type = OPR_REG;
-	DX.data_size = 16;
-	DX.addr = 2;
+	dx.type = OPR_REG;
+	dx.data_size = 16; //%dx
+	dx.addr = 2;
+	operand_read(&dx); //get %dx
+
+	al.type = OPR_REG;
+	al.data_size = 8; //%al
+	al.addr = 0;
+	al.val = pio_read(dx.val, 1); //Input byte from port dx into al
 	return 1;
 }
