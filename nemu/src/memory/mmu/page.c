5f52640cbf76d8871bc140bdc2b64d6eb2c8b877
tracer-pa2018-fall <tracer@njuics.org>
2019-01-05 13:49:58 +0800

    >  clean 171860699 lixiao Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  13:49:58 up 12:22,  1 user,  load average: 0.17, 0.14, 0.05 fb858b9976ffcc5f29a559b0c2cec3b21bd27c92

--- nemu/src/memory/mmu/page.c
+++ nemu/src/memory/mmu/page.c
@@ -14,7 +14,7 @@ paddr_t page_translate(laddr_t laddr) {
 	PDE pde;
 	pde.val = paddr_read(PDE_start, 4); // read PDE
 //printf("pde.val:%x\n", pde.val);
-if(pde.present != 1) printf("laddr:%x\neip:%x\neax:%x\nebp:%x", laddr, cpu.eip, cpu.eax, cpu.ebp);
+if(pde.present != 1) printf("laddr:%x\neip:%x\neax:%x\nebp:%x\npde.val:%x\n", laddr, cpu.eip, cpu.eax, cpu.ebp, pde.val);
 	assert(pde.present == 1);
 
 	paddr_t PTE_start = (pde.page_frame << 12) + 4 * page; // page table start here.
@@ -22,7 +22,7 @@ if(pde.present != 1) printf("laddr:%x\neip:%x\neax:%x\nebp:%x", laddr, cpu.eip, 
 	PTE pte;
 	pte.val = paddr_read(PTE_start, 4); // read PTE
 //printf("pte.val:%x\n", pte.val);
-if(pte.present != 1) printf("laddr:%x\neip:%x\neax:%x\nebp:%x", laddr, cpu.eip, cpu.eax, cpu.ebp);
+if(pte.present != 1) printf("laddr:%x\neip:%x\neax:%x\nebp:%x\npte.val:%x\n", laddr, cpu.eip, cpu.eax, cpu.ebp, pte.val);
 	assert(pte.present == 1);
 
 	paddr_t paddr = (pte.page_frame << 12) + offset; // physical addr.
