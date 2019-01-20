f9c580b590a3dfabe5de988cd6241073dbb97b8b
monitor <tracer@njuics.org>
2019-01-20 18:29:01 +0800

    monitor test

--- kernel/src/fs/fs.c
+++ kernel/src/fs/fs.c
@@ -108,7 +108,7 @@ off_t fs_lseek(int fd, off_t offset, int whence) {
 						break;
 		default: return -1;
 	}
-	return files[fd].offset - file_table[fd - 3].disk_offset;
+	return (files[fd].offset - file_table[fd - 3].disk_offset);
 }
 
 int fs_close(int fd) {
