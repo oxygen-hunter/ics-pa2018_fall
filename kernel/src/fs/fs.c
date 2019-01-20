82282b002f9d8295d94285ff4f8bb56038bc994a
tracer-pa2018-fall <tracer@njuics.org>
2019-01-20 11:52:22 +0800

    >  nemu 171860699 lixiao Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  11:52:22 up  2:36,  1 user,  load average: 0.02, 0.03, 0.00 fbc1ee59ee56b2764e09f5aa6e43557bea23a3ac

--- kernel/src/fs/fs.c
+++ kernel/src/fs/fs.c
@@ -90,12 +90,14 @@ size_t fs_write(int fd, void *buf, size_t len) {
 off_t fs_lseek(int fd, off_t offset, int whence) {
 	//panic("Please implement fs_lseek at fs.c");
 	uint32_t size = file_table[fd - 3].size;
-	uint32_t boundary = file_table[fd - 3].disk_offset + size;
+	uint32_t boundary1 = file_table[fd - 3].disk_offset + size;
+	uint32_t boundary2 = file_table[fd - 3].disk_offset + size;
+	uint32_t boundary3 = file_table[fd - 3].disk_offset + size;
 	switch(whence) {
-		case SEEK_SET: files[fd].offset = offset; assert(files[fd].offset < boundary); break;
-		case SEEK_CUR: files[fd].offset += offset; assert(files[fd].offset < boundary); break;
+		case SEEK_SET: files[fd].offset = offset; assert(files[fd].offset < boundary1); break;
+		case SEEK_CUR: files[fd].offset += offset; assert(files[fd].offset < boundary2); break;
 		case SEEK_END: files[fd].offset = file_table[fd - 3].disk_offset + file_table[fd - 3].size + offset;
-						assert(files[fd].offset < boundary); break;
+						assert(files[fd].offset < boundary3); break;
 		default: return -1;
 	}
 	return files[fd].offset - file_table[fd - 3].disk_offset;
