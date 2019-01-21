37784cda9e893a8bd995fc7b55cc862256e2e93b
monitor <tracer@njuics.org>
2019-01-21 11:27:01 +0800

    monitor test

--- game/src/nemu-pal/hal/timer.c
+++ game/src/nemu-pal/hal/timer.c
@@ -34,6 +34,8 @@ uint32_t SDL_GetTicks() {
 void SDL_Delay(uint32_t ms) {
 	/* TODO: Return from this function after waiting for `ms' milliseconds. */
 	//assert(0);
-	uint32_t nr_delay_T = ms / 10;
-	jiffy = jiffy + nr_delay_T;
+	uint32_t delay_begin = SDL_GetTicks();
+	while(SDL_GetTicks - delay_begin < ms) {
+			//do nothing but wait delay
+	}
 }
