d2b35341f5606a9eb238fd9801470497173b56c2
tracer-pa2018-fall <tracer@njuics.org>
2019-01-21 10:14:40 +0800

    >  nemu 171860699 lixiao Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  10:14:40 up  2:29,  1 user,  load average: 0.19, 0.26, 0.17 d8309f26d6ceb17d579a1bd8ef04370c8d14cf3

--- game/src/nemu-pal/hal/timer.c
+++ game/src/nemu-pal/hal/timer.c
@@ -34,6 +34,6 @@ uint32_t SDL_GetTicks() {
 void SDL_Delay(uint32_t ms) {
 	/* TODO: Return from this function after waiting for `ms' milliseconds. */
 	//assert(0);
-	uint32_t nr_delay_T = ms / 10;
-	jiffy = jiffy + nr_delay_T;
+	//uint32_t nr_delay_T = ms / 10;
+	//jiffy = jiffy + nr_delay_T;
 }
